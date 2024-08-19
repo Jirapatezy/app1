5489135028a9b0dffbf447a976fc41c1d812a536
Jirapatezy <104245600+Jirapatezy@users.noreply.github.com>
2024-08-19 09:48:58 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Calendar from './class-components'
+import { Header,Content,Footer } from './func-components'
+
+export default function App(){
+  return(
+    [<Header />,<Content />,<Footer />]
+    )
+
+  }
+  
+
---
+++ src/refs-func.js
@@ -0,0 +1,5 @@
+import React,{useRef} from "react";
+export default function RefsFunc(){
+    const num1=useRef()
+    
+}
